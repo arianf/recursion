37ea4f64e290ab95c1f9173742d29a7d25c3c0a4
Arian Faurtosh <arian@icloud.com>
2015-01-15 16:39:32 -0800

    have some viable code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,52 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  
+  var elements = [];
+  var body;
+  var current;
+
+  var eval = function (){
+    if( typeof(body) == 'undefined' ){
+      body = document.body;
+      eval();
+    }else{
+      body = body.childNodes;
+
+      // body.childNodes[3].nodeName.toLowerCase()
+      for (var i = 0; i < body.length; i++) {
+        
+        console.log(i);
+
+
+        if(typeof(body[i].classList) != 'undefined'){
+          console.log(body[i]);
+          console.log(body[i].classList.contains(className));
+          if(body[i].classList.contains(className)){
+            console.log("TEST");
+            elements.push(body[i].nodeName.toLowerCase() + '.' + className);
+            console.log("TEST2");
+            console.log(elements);
+          }
+        }
+        //   for(var x = 0; x < body[i].childNodes[i].classList.length; x++){
+        //     if(body[i].childNodes[i].classList[x] == className){
+        //       elements.push(body.childNodes[i].nodeName.toLowerCase() + '.' + className);
+        //     }
+        //   }
+        // }
+        // if(body[i].childNodes[i].length != 0){
+        //   eval();
+        // }
+      }
+    }
+  }
+  eval();
+  console.log("END: ");
+  console.log(elements);
+  return elements;
+  // return ['body.targetClassName', 'p.targetClassName'];
+
+  // return array element.class
 };

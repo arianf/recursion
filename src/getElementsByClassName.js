2c7b16eeff33a75d256bfed24c3e9188bcacf80f
Arian Faurtosh <arian@icloud.com>
2015-01-15 18:25:18 -0800

    can't figure out why the entire function is ran 7 times

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,42 +5,30 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  
   var elements = [];
   var body = document.body;
-  // var current;
 
-  var eval = function (current){
-    
-    /* If current containts, push */
+  var repeat = function (current){
+    // console.log(current);
+
+    /* Check to see if element is not text */
     if(typeof(current.classList) != 'undefined'){
+
+      /* Check to see if elemnt containts className */
       if(current.classList.contains(className)){
         elements.push(current);
       }
 
       current = current.childNodes;
+      /* Cycle through current's nodes, using current array length */
       for (var i = 0; i < current.length; i++) {
-        eval(current[i]);
+        repeat(current[i]);
       }
     }
+  };
+  
+  repeat(body);
 
-      // current = current.childNodes;
-
-      /* Cycle through current list, using current array length */
-      // for (var i = 0; i < current.length; i++) {
-
-        /* Check to see if element is not text */
-        // if(typeof(current[i].classList) != 'undefined'){
-
-          /* Check to see if elemnt containts className */
-          // if(current[i].classList.contains(className)){
-            // elements.push(current[i]);
-          // }
-          // eval(current[i]);
-        // }
-      // }
-  }
-  eval(body);
-  console.log(elements);
+  console.log('run test');
   return elements;
 };

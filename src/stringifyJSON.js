1566cb9b96f93481fc03c4fb131b43cd1a85e7c4
Arian Faurtosh <arian@icloud.com>
2015-01-17 16:54:24 -0800

    FIXED INDENTING

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,43 +7,43 @@ var stringifyJSON = function(obj) {
   // your code goes here
   
   // if(obj == null){
-  // 	return "null";
+  //   return "null";
   // }else if(typeof(obj) == 'string' || typeof(obj)  == 'object'){
-  // 	return '"'+obj.toString()+'"';
+  //   return '"'+obj.toString()+'"';
   // }
   var stack = [];
   var repeat = function(current) {
-  	if(typeof(current) === 'string'){
-  		stack.push('"'+current+'"');
-  	}else if(current == null){
-  		stack.push('null');
-  	}else if($.isArray(current)){
-  		stack.push('[');
-  		for (var i = 0; i < current.length; i++){
-  			repeat(current[i]);
-  			if(current.length-1 != i){
-  				stack.push(',');
-  			}
-  		}
-  		stack.push(']');
-  	}else if($.isPlainObject(current)){
-  		stack.push('{');
+    if(typeof(current) === 'string'){
+      stack.push('"'+current+'"');
+    }else if(current == null){
+      stack.push('null');
+    }else if($.isArray(current)){
+      stack.push('[');
+      for (var i = 0; i < current.length; i++){
+        repeat(current[i]);
+        if(current.length-1 != i){
+          stack.push(',');
+        }
+      }
+      stack.push(']');
+    }else if($.isPlainObject(current)){
+      stack.push('{');
 
-  		for(var prop in current){
-  			if(typeof(current[prop]) != 'undefined' && typeof(current[prop]) != 'function'){
-	  			stack.push('"'+prop + '":');
-	  			repeat(current[prop]);
-	  			stack.push(',');
-	  		}
-  		}
-  		if(stack[stack.length-1] == ','){
-  			stack.pop();
-  		}
+      for(var prop in current){
+        if(typeof(current[prop]) != 'undefined' && typeof(current[prop]) != 'function'){
+          stack.push('"'+prop + '":');
+          repeat(current[prop]);
+          stack.push(',');
+        }
+      }
+      if(stack[stack.length-1] == ','){
+        stack.pop();
+      }
 
-  		stack.push('}');
-  	}else{
-  		stack.push(current);
-  	}
+      stack.push('}');
+    }else{
+      stack.push(current);
+    }
   };
   repeat(obj);
   return stack.join("");
